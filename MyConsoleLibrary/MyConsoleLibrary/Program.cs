183b9d468bf8ed94843848d256f113c7df0e931e
ST10033808 <LeighcheLeighche28@gmail.com>
2024-04-09 09:29:39 +0200

    Application Code

---
+++ MyConsoleLibrary/MyConsoleLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace MyConsoleLibrary
+{
+    class BookLibrary
+    {
+
+        public static void Main()
+        {
+            BookMethods callPrompts = new BookMethods();
+
+            callPrompts.BookPrompts();
+
+            while (true)
+            {
+                Console.WriteLine("\nWhat would you like to do\n\n" +
+                "1.Read Book\n" +
+                "2.Scale Pages\n" +
+                "3.Revert Pages\n" +
+                "4.Delete Book\n" +
+                "5.Exit\n");
+
+                int userChoice = int.Parse(Console.ReadLine());
+
+
+                switch (userChoice)
+                {
+                    case 1:
+                        callPrompts.DisplayBook();
+                        break;
+
+                    case 2:
+                        callPrompts.IncreasePages();
+                        break;
+
+                    case 3:
+                        callPrompts.RevertPage();
+                        break;
+
+                    case 4:
+                        callPrompts.ClearShelve();
+                        break;
+
+                    case 5:
+                        callPrompts.CloseBook();
+                        break;
+                }
+                continue;
+            }
+        }
+    }
+}
+
